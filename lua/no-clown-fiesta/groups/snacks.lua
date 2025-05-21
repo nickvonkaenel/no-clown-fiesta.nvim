b6b505e4bed7d9aa89cf0e16b0d0c428d77f4b15
nvk <1234+nickvonkaenel@users.noreply.github.com>
2025-05-21 14:32:59 -0700

    snacks indent

--- lua/no-clown-fiesta/groups/snacks.lua
+++ lua/no-clown-fiesta/groups/snacks.lua
@@ -4,8 +4,16 @@ function M.highlight(palette, opts)
   return {
     SnacksPickerMatch = { fg = palette.orange },
     SnacksIndent = { fg = palette.border_dark },
-    SnacksIndentScope = { fg = palette.border_light },
-    SnacksIndentChunk = { fg = palette.border_light },
+    SnacksIndentScope = { fg = "#646b79" },
+    SnacksIndentChunk = { fg = "#646b79" },
+    SnacksIndent1 = { fg = "#2a2e33" },
+    SnacksIndent2 = { fg = "#31353b" },
+    SnacksIndent3 = { fg = "#383c44" },
+    SnacksIndent4 = { fg = "#3f444c" },
+    SnacksIndent5 = { fg = "#464b55" },
+    SnacksIndent6 = { fg = "#4d535e" },
+    SnacksIndent7 = { fg = "#545b67" },
+    SnacksIndent8 = { fg = "#5c6370" },
   }
 end
 
